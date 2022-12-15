f1772fdd54f5d859cb2d4d2de612b784ebfe467e
EcaCosca <enriquecoscarelli@gmail.com>
2022-12-15 10:22:07 +0100

    Second example done

--- index.js
+++ index.js
@@ -2,13 +2,39 @@ const express = require('express');
 
 const app = express()
 
+// MIDDLEWARE
+// ANY request that comes in, checks if theres a "body" and attaches it to the request
+app.use(express.json())
+
+// FIRST EXAMPLE USING NEXT
+
 app.get('/', (req, res, next)=>{
-    console.log("Hello there!")
     next()
+    res.send("Hello there!")
 })
 
 app.get('/', (req, res, next)=>{
-    res.send("Ok bye now!")
+    console.log("Ok bye now!")
 })
 
-app.listen(4500, ()=>{console.log("Connected")})
+
+// EXAMPLE NUMERO DOS 
+
+// const getOnlyMiddleware = (request, response, next) => {
+    //     if(request.method === 'GET') { // processes the 
+    //         console.log("Inside of if statemente")
+    //         next();	// request and calls next if method is GET
+    //     } else {
+        //         console.log("Inside of else statemente")
+        //      return response.sendStatus(403); // wrong method used, ends req/res cycle with an error message
+        //     }
+        // };
+        
+        // app.use('/path', getOnlyMiddleware);
+        
+app.post('/body', (req, res, next)=>{
+    res.send(req.body)
+})
+        
+        
+        app.listen(4500, ()=>{console.log("Connected")})
